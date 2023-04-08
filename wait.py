6db57197d6a99ac56869ad9d99ea999695af734a
adiyanislam <42025128+adiyanislam@users.noreply.github.com>
2023-04-08 13:07:11 -0700

    Create wait.py

---
+++ wait.py
@@ -0,0 +1,22 @@
+"""
+wait
+
+Description:
+"""
+time = 1150
+def wait(): 
+    x = 0
+    while x <= time:
+        x += 1
+        z = 5
+        c = z ^ 100
+        i = int(c/c)
+        i = i ^ 100
+        p = i ^1000
+        o= 9
+        p= o^2
+        o = p^2
+        t = 222
+        t= t^2
+        x -= .9
+        
